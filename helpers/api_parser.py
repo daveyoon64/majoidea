1efe77e2e7a7d8d35d80b193a77f07420c8da050
Raid55 <boutboulnicholas@gmail.com>
2018-05-03 21:52:32 +0000

    fixed api parser and fixed a few other lines

--- helpers/api_parser.py
+++ helpers/api_parser.py
@@ -1,8 +1,5 @@
-import json
 
-
-def parse_movie(json):
-    movie = json.loads(json)
+def parse_movie(movie):
     return {
         "cast": [o['name'] for o in movie['cast'] if o['name'] != ""],
         "title": movie['title'],
@@ -10,9 +7,9 @@ def parse_movie(json):
         "poster": movie['poster']['large']
     }
 
-def parse_actor(json):
-    actor = json.loads(json)[0]
-    if "actor" in actor['filmography']
+def parse_actor(actorArr):
+    actor = actorArr[0]
+    if "actor" in actor['filmography']:
         act = "actor"
     else:
         act= "actress"
@@ -20,7 +17,7 @@ def parse_actor(json):
         "name": actor['title'],
         "id": actor['person_id'],
         "filmography": [{
-            "id": o['imbd_id'],
+            "id": o['imdb_id'],
             "title": o['title'],
             "year": o['year']
         } for o in actor['filmography'][act] if o['type'] == "Film"]
